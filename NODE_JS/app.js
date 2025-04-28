81d2c8cf8174cde881dad95616400310b3e79bff
msi-end <aditya01377@gmail.com>
2025-04-28 06:53:09 +0530

    new updates of css and js

--- NODE_JS/app.js
+++ NODE_JS/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const compression = require("compression");
 const path = require("path");
 const fs = require("fs");
+const { minify } = require("terser"); 
+const CleanCSS = require("clean-css"); 
 
 const app = express();
 const port = 3019;
@@ -11,43 +13,47 @@ app.use(compression());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+app.use(async (req, res, next) => {
+  if (req.url.endsWith(".js")) {
+    const filePath = path.join(__dirname, "public", req.url);
+    try {
+      const jsContent = fs.readFileSync(filePath, "utf8");
+      const minified = await minify(jsContent);
+      res.setHeader("Content-Type", "application/javascript");
+      res.setHeader("Cache-Control", "public, max-age=86400"); 
+      res.send(minified.code);
+    } catch (err) {
+      next();
+    }
+  } else if (req.url.endsWith(".css")) {
+    const filePath = path.join(__dirname, "public", req.url);
+    try {
+      const cssContent = fs.readFileSync(filePath, "utf8");
+      const minified = new CleanCSS().minify(cssContent);
+      res.setHeader("Content-Type", "text/css");
+      res.setHeader("Cache-Control", "public, max-age=86400");
+      res.send(minified.styles);
+    } catch (err) {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
 app.use(express.static(path.join(__dirname, "public"), { maxAge: '1d' }));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/fruits", (req, res) => {
-  res.render("fruits");
-});
-
-app.get("/about-us", (req, res) => {
-  res.render("about-us");
-});
-
-app.get("/vegetables", (req, res) => {
-  res.render("vegetables");
-});
-
-app.get("/rice-corn", (req, res) => {
-  res.render("rice-corn");
-});
-
-app.get("/northeast-source", (req, res) => {
-  res.render("northeast-source");
-});
-
-app.get("/social-impact", (req, res) => {
-  res.render("social-impact");
-});
-
-app.get("/cultivate-northeast", (req, res) => {
-  res.render("cultivate-northeast");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+// Routes
+app.get("/", (req, res) => res.render("index"));
+app.get("/fruits", (req, res) => res.render("fruits"));
+app.get("/about-us", (req, res) => res.render("about-us"));
+app.get("/vegetables", (req, res) => res.render("vegetables"));
+app.get("/rice-corn", (req, res) => res.render("rice-corn"));
+app.get("/northeast-source", (req, res) => res.render("northeast-source"));
+app.get("/social-impact", (req, res) => res.render("social-impact"));
+app.get("/cultivate-northeast", (req, res) => res.render("cultivate-northeast"));
+app.get("/contact", (req, res) => res.render("contact"));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
